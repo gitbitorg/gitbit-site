a32711420c368d0d5ef1974177f54952dc4d7705
John Gruber <john.gruber@gitbit.org>
2018-04-09 19:56:23 -0400

    added sitemap

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp')
 const clean = require('gulp-clean')
 const pug = require('gulp-pug')
+const sitemap = require('gulp-sitemap')
 
-gulp.task('default', ['clean', 'views'])
+gulp.task('default', ['clean', 'views', 'sitemap'])
 
 gulp.task('clean', () => gulp.src('docs/**.html', {force: true}).pipe(clean()))
 
@@ -10,4 +11,14 @@ gulp.task('views', () =>
   gulp.src('views/pages/*.pug')
     .pipe(pug({ }))
     .pipe(gulp.dest('docs'))
+)
+
+gulp.task('sitemap', () =>
+  gulp.src('docs/**/*.html', {
+    read: false
+  })
+    .pipe(sitemap({
+      siteUrl: 'http://gitbit.org'
+    }))
+    .pipe(gulp.dest('./docs'))
 )
