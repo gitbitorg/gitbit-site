a38a645466ef682c6cb66cc9e2109f080cd07814
John Gruber <john.gruber@gitbit.org>
2018-04-10 21:32:39 -0400

    fixed rss feed item url

--- views/articles.js
+++ views/articles.js
@@ -1,34 +1,34 @@
 module.exports = [
   {
-    link: 'https://blog.usejournal.com/10-reasons-sharepoint-online-destroys-file-shares-7c2c2680f1e9',
+    url: 'https://blog.usejournal.com/10-reasons-sharepoint-online-destroys-file-shares-7c2c2680f1e9',
     image: 'assets/SharePoint-vs-File-Shares-original.jpg',
     title: '10 Reasons SharePoint Online Destroys File Shares',
     description: 'The IT department of the future leads the business driving innovation. We streamline the day to day, improve customer experience, and help the sales team close more deals!',
     date: 'Febuary 1, 2018'
   },
   {
-    link: 'https://medium.com/swlh/how-to-stop-managing-and-start-meeting-deadlines-d404f65404a9',
+    url: 'https://medium.com/swlh/how-to-stop-managing-and-start-meeting-deadlines-d404f65404a9',
     image: 'assets/Office-365-Planner-300x200.png',
     title: 'How to Stop Managing and Start Meeting Deadlines',
     description: 'Scrum is the leading agile development methodology, used by Fortune 500 companies around the world. Planner is Microsoft Office 365’s task management app designed for agile teams.',
     date: 'January 24, 2018'
   },
   {
-    link: 'https://medium.com/@gruberjl/13-outlook-productivity-organization-tips-244e9f16efeb',
+    url: 'https://medium.com/@gruberjl/13-outlook-productivity-organization-tips-244e9f16efeb',
     image: 'assets/Outlook-2016-300x200.jpg',
     title: '13 Outlook Productivity & Organization Tips',
     description: 'Microsoft Outlook is a personal information manager from Microsoft, accessible as a piece of the Microsoft Office suite. Albeit often used as an email application, it likewise incorporates a…',
     date: 'January 23, 2018'
   },
   {
-    link: 'https://hackernoon.com/19-reasons-exchange-online-is-winning-195f2188150d',
+    url: 'https://hackernoon.com/19-reasons-exchange-online-is-winning-195f2188150d',
     image: 'assets/Exchange-Online-300x200.jpg',
     title: '19 Reasons Exchange Online is Winning',
     description: 'Exchange Online is Microsoft’s hosted email for businesses. It’s the industry-leading cloud solution adopted by small businesses and fortune 500 companies. Microsoft Exchange Online provides the…',
     date: 'January 19, 2018'
   },
   {
-    link: 'https://medium.com/@gruberjl/10-tips-to-improve-productivity-using-onenote-85dee4a32cf2',
+    url: 'https://medium.com/@gruberjl/10-tips-to-improve-productivity-using-onenote-85dee4a32cf2',
     image: 'assets/OneNote-2016-300x200.jpg',
     title: '11 Tips for Improving Productivity using OneNote',
     description: 'OneNote is a digital notebook that automatically backs up to Microsoft’s Office 365 cloud. Microsoft has developed apps for every device including Windows PC, Mac,iPhone, Android. OneNote notebooks…',
@@ -38,7 +38,7 @@ module.exports = [
 
 /*
 {
-  link: '',
+  url: '',
   image: '',
   title: '',
   body: '',
