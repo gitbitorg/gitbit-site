690cf5cb367f2675d23e1478a3125dc3bb0ebb12
John Gruber <john.gruber@gitbit.org>
2018-04-24 16:42:52 -0400

    added amp

--- build.js
+++ build.js
@@ -1,6 +1,9 @@
+/* eslint no-unused-vars:0 */
+
 const fs = require('fs')
 const Glob = require('glob').Glob
 const pug = require('pug')
+const ampify = require('ampify')
 
 const clean = () => {
   const {found} = Glob('docs/**.html', {sync:true})
@@ -14,9 +17,12 @@ const views = () => {
   const {found} = Glob('views/pages/**/*.pug', {sync:true})
   found.forEach((file) => {
     const html = pug.renderFile(file, {articles})
-    console.log(html)
+    const amp = ampify(html, {cwd:'./docs'})
+
+    const destinationPath = file.replace('views/pages/', './docs/').replace('.pug', '.html')
+    fs.writeFileSync(destinationPath, amp)
   })
 }
 
 // clean()
-views()
+// views()
