721629d9c87c4570d8c9a44cb65ea42c2319b032
John Gruber <john.gruber@gitbit.org>
2018-04-24 21:53:14 -0400

    added jsonld

---
+++ docs/assets/article/bookings/jsonld.js
@@ -0,0 +1,35 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org",
+  "@type": "TechArticle",
+  "mainEntityOfPage": {
+    "@type": "WebPage",
+    "@id": "http://gitbit.org/articles/how-to-automate-scheduling-with-office-365.html"
+  },
+  "headline": "How to Automate scheduling with Office 365",
+  "image": [
+    "http://gitbit.org/assets/article/bookings/How-to-Automate-Scheduling-with-Office-365.jpg",
+    "http://gitbit.org/assets/article/bookings/How-to-Automate-Scheduling-with-Office-365-1200x900.jpg",
+    "http://gitbit.org/assets/article/bookings/How-to-Automate-Scheduling-with-Office-365-1200x1200.png"
+   ],
+  "datePublished": "2018-04-24T05:00:00Z",
+  "dateModified": "2018-04-24T05:00:00Z",
+  "author": {
+    "@type": "Person",
+    "name": "John Gruber",
+    "email": "john.gruber@gitbit.org"
+  },
+   "publisher": {
+    "@type": "Organization",
+    "name": "GitBit",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "http://gitbit.org/assets/branding/gitbit-logo-200x60.jpg"
+    }
+  },
+  "description": "Office 365 has a Bookings app included. Use Bookings to automate and streamline your scheduling.",
+  "about": "Office 365",
+  "wordCount": 1500,
+  "keywords": "Office 365, Microsoft Bookings, Online Scheduling"
+}
+</script>
