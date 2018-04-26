b85fc07b035d39b2f4198241d5fab2225ecaf833
John Gruber <john.gruber@gitbit.org>
2018-04-26 10:23:59 -0400

    added basic draft converter

--- publisher.js
+++ publisher.js
@@ -1,5 +1,49 @@
-var minify = require('html-minifier').minify
-var result = minify('<p title="blah" id="moo">foo</p>', {
-  removeAttributeQuotes: true
+const mammoth = require('mammoth')
+const fs = require('fs')
+
+const assetsFolder = 'test'
+const path = './Title1.docx'
+
+const encode = (str) => {
+  str = str.replace(/[^a-zA-Z 0-9]/g, '')
+  str = str.replace(/ /g, '-')
+  str = str.substring(0, 15)
+  return str
+}
+
+const createDir = () => {
+  if (!fs.existsSync(`${__dirname}/${assetsFolder}`)) fs.mkdirSync(`${__dirname}/${assetsFolder}`)
+  return assetsFolder
+}
+
+const createUniqueFileName = (path, fileName, extension) => {
+  if (!fs.existsSync(`${path}${fileName}.${extension}`)) return fileName
+
+  let num = 0
+  while (fs.existsSync(`${path}${fileName}${num}.${extension}`)) {
+    num++
+  }
+
+  return `${fileName}${num}`
+}
+
+const convertImage = mammoth.images.imgElement(async (image) => {
+  if (!image.altText) throw 'imported document has an image without altText.'
+
+  const extension = image.contentType.split('/')[1]
+
+  const folderName = createDir()
+  const fileName = createUniqueFileName(`${__dirname}/${folderName}/`, encode(image.altText), extension)
+
+  const buf = await image.read()
+  fs.writeFileSync(`${__dirname}/${folderName}/${fileName}.${extension}`, buf)
+  return { src: `/${folderName}/${fileName}.${extension}` }
 })
-console.log(result)
+
+const start = async () => {
+  const res = await mammoth.convertToHtml({path}, {convertImage})
+  console.log(res.value)
+  console.log(res.messages)
+}
+
+start()
