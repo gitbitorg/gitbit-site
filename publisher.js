867d118b9a4f172ac416f7578aab4b8d4da4d360
John Gruber <john.gruber@gitbit.org>
2018-04-27 12:50:38 -0400

    made changes to article meta

--- publisher.js
+++ publisher.js
@@ -1,8 +1,16 @@
 const mammoth = require('mammoth')
 const fs = require('fs')
+const html2pug = require('html2pug')
 
-const assetsFolder = 'OneNote'
 const path = './drafts/11 Tips for Improving Productivity using.docx'
+
+const article = {
+  image: '/assets/article/bookings/Microsoft-Bookings-Screenshot-300x200.png',
+  title: '11 Tips for Improving Productivity using OneNote',
+  description: '',
+  keywords: '',
+  assetsFolder: ''
+}
 
 const encode = (str) => {
   str = str.replace(/[^a-zA-Z 0-9]/g, '')
@@ -12,9 +20,9 @@ const encode = (str) => {
 }
 
 const createDir = () => {
-  if (!fs.existsSync(`${__dirname}/docs/assets/article/${assetsFolder}`))
-    fs.mkdirSync(`${__dirname}/docs/assets/article/${assetsFolder}`)
-  return assetsFolder
+  if (!fs.existsSync(`${__dirname}/docs/assets/article/${article.assetsFolder}`))
+    fs.mkdirSync(`${__dirname}/docs/assets/article/${article.assetsFolder}`)
+  return article.assetsFolder
 }
 
 const createUniqueFileName = (path, fileName, extension) => {
@@ -41,10 +49,32 @@ const convertImage = mammoth.images.imgElement(async (image) => {
   return { src: `/assets/article/${folderName}/${fileName}.${extension}` }
 })
 
+const writeMeta = () => {
+  article.fileName = encode(article.title)
+  article.datePublished = (new Date()).toISOString()
+  article.dateModified = (new Date()).toISOString()
+  article.canonical = `/articles/${article.fileName}`
+
+  /*
+    canonical
+    description
+    keywords
+    image1x1
+    image16x9
+    image4x3
+  */
+}
+
 const start = async () => {
   const res = await mammoth.convertToHtml({path}, {convertImage})
-  // console.log(res.value)
-  console.log(res.messages)
+  const pug = html2pug(res.value, { tabs: true })
+  if (res.messages.length > 0) {
+    console.log('article not published')
+    console.log(res.messages)
+  } else {
+    writeMeta()
+    fs.writeFileSync(`${__dirname}/views/pages/articles/${article.fileName}.pug`, pug)
+  }
 }
 
 start()
