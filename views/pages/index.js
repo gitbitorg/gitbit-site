898767384388635bcd698ab21accf9e7725a5f0e
John Gruber <john.gruber@gitbit.org>
2018-04-27 21:46:05 -0400

    added meta to index

---
+++ views/pages/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'title': 'GitBit | Office 365 Tips to Improve Productivity',
+  'description': 'Over 70% of Fortune 500 companies have migrated to Microsoft Office 365. Learn the latest tips to maximize your ROI.',
+  'keywords': 'Office 365, Microsoft, GitBit',
+  'assetsFolder': 'branding',
+  'fileName': 'index',
+  'datePublished': '2018-04-20T00:00:00.000Z',
+  'dateModified': '2018-04-28T01:24:32.313Z',
+  'canonical': '/',
+  'image1200x1200': 'http://gitbit.org/assets/branding/GitBit-Logo-1200x1200.png',
+  'image1200x900': 'http://gitbit.org/assets/branding/GitBit-Logo-1200x900.png',
+  'image1200x675': 'http://gitbit.org/assets/branding/GitBit-Logo-1200x675.png',
+  'image400x300': 'http://gitbit.org/assets/branding/400x300.jpg'
+}
