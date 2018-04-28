72bbf9fdb859003fa22b28629ef3164a01ba041d
John Gruber <john.gruber@gitbit.org>
2018-04-27 20:34:18 -0400

    updated to include featured image

--- publisher.js
+++ publisher.js
@@ -1,8 +1,8 @@
 const mammoth = require('mammoth')
 const fs = require('fs')
 const html2pug = require('html2pug')
-
-const path = './drafts/11 Tips for Improving Productivity using.docx'
+const sharp = require('sharp')
+const Glob = require('glob').Glob
 
 const article = {
   title: '11 Tips for Improving Productivity using OneNote',
@@ -50,7 +50,7 @@ const convertImage = mammoth.images.imgElement(async (image) => {
 })
 
 const writeMeta = () => {
-  article.fileName = encode(article.title)
+  article.fileName = encode(article.title, false)
   article.datePublished = (new Date()).toISOString()
   article.dateModified = (new Date()).toISOString()
   article.canonical = `/articles/${article.fileName}`
@@ -63,13 +63,38 @@ const writeMeta = () => {
   fs.writeFileSync(`${__dirname}/views/pages/articles/${article.fileName}.js`, output)
 }
 
+const createThumbnail = () => sharp(`./docs/assets/article/${article.assetsFolder}/1200x900.jpg`)
+  .resize(400, 300)
+  .sharpen()
+  .toFile(`./docs/assets/article/${article.assetsFolder}/400x300.jpg`)
+
+
+const moveFiles = () => {
+  const {found} = Glob('./drafts/*.*', {sync:true})
+  found.forEach((file) => {
+    fs.renameSync(file, file.replace('./drafts/', `${__dirname}/docs/assets/article/${article.assetsFolder}/`))
+  })
+}
+
+const getDocx = () => {
+  const {found} = Glob('./drafts/*.docx', {sync:true})
+  if (found.length > 1) {
+    throw 'multiple drafts in dradt folder. Please work one draft at a time'
+  }
+
+  return found[0]
+}
+
 const start = async () => {
-  const res = await mammoth.convertToHtml({path}, {convertImage})
+  const pathToDraft = getDocx()
+  const res = await mammoth.convertToHtml({path: pathToDraft}, {convertImage})
   const pug = html2pug(res.value, { tabs: true })
   if (res.messages.length > 0) {
     console.log('article not published')
     console.log(res.messages)
   } else {
+    moveFiles()
+    await createThumbnail()
     writeMeta()
     fs.writeFileSync(`${__dirname}/views/pages/articles/${article.fileName}.pug`, pug)
   }
