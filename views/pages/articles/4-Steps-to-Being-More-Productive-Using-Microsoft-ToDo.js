fc7ac124fc9938a3baa56f0b5fac2fc4e96bf8fb
John Gruber <john.gruber@gitbit.org>
2018-05-09 14:01:20 -0400

    added new article 4 steps to productivity using To-Do

---
+++ views/pages/articles/4-Steps-to-Being-More-Productive-Using-Microsoft-ToDo.js
@@ -0,0 +1,14 @@
+module.exports = {
+	"title": "4 Steps to Being More Productive Using Microsoft To-Do",
+	"description": "\"The list is the origin of culture. What does culture want? To make infinity comprehensible. And how does one face infinity? Through lists\"",
+	"keywords": "Office 365, Microsoft To-Do, Productivity",
+	"assetsFolder": "Productivity-To-Do",
+	"fileName": "4-Steps-to-Being-More-Productive-Using-Microsoft-ToDo",
+	"datePublished": "2018-05-09T17:43:04.401Z",
+	"dateModified": "2018-05-09T17:43:04.401Z",
+	"image1200x1200": "http://gitbit.org/assets/article/Productivity-To-Do/1200x1200.jpg",
+	"image1200x900": "http://gitbit.org/assets/article/Productivity-To-Do/1200x900.jpg",
+	"image1200x675": "http://gitbit.org/assets/article/Productivity-To-Do/1200x675.jpg",
+	"image400x300": "http://gitbit.org/assets/article/Productivity-To-Do/400x300.jpg",
+	"canonical": "/articles/4-Steps-to-Being-More-Productive-Using-Microsoft-ToDo"
+}
