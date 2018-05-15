a00158cc2bbb0e1fe1c3880d65867aaed5b7d7d4
John Gruber <john.gruber@gitbit.org>
2018-05-15 07:35:27 -0400

    Added article

---
+++ views/pages/articles/Agile-Teamwork-with-Office-365-Planner.js
@@ -0,0 +1,14 @@
+module.exports = {
+	"title": "Agile Teamwork with Office 365 Planner",
+	"description": "How to improve productivity and organization using Microsoft Office 365's Planner app.",
+	"keywords": "Microsoft Planner, Scrum, Project Management",
+	"assetsFolder": "Microsoft-Planner-Intro",
+	"fileName": "Agile-Teamwork-with-Office-365-Planner",
+	"datePublished": "2018-05-15T11:34:51.272Z",
+	"dateModified": "2018-05-15T11:34:51.272Z",
+	"image1200x1200": "http://gitbit.org/assets/article/Microsoft-Planner-Intro/1200x1200.jpg",
+	"image1200x900": "http://gitbit.org/assets/article/Microsoft-Planner-Intro/1200x900.jpg",
+	"image1200x675": "http://gitbit.org/assets/article/Microsoft-Planner-Intro/1200x675.jpg",
+	"image400x300": "http://gitbit.org/assets/article/Microsoft-Planner-Intro/400x300.jpg",
+	"canonical": "/articles/Agile-Teamwork-with-Office-365-Planner"
+}
