ff4608af7725156d97cbdc7ad61409789ca4e72b
John Gruber <john.gruber@gitbit.org>
2018-05-18 13:36:50 -0400

    added more twitter posts to smm

--- build/smm.js
+++ build/smm.js
@@ -2,17 +2,18 @@ const {resolve} = require('path')
 const {renderTemplate, getMeta, getAbsoluteUrl} = require('./render-template')
 const cheerio = require('cheerio')
 const BitlyClient = require('bitly')
-const {Apps} = require(resolve(__dirname, '..', '..', 'workplace', 'packages', 'sharepoint'))
+const {Apps, Keywords} = require(resolve(__dirname, '..', '..', 'workplace', 'packages', 'sharepoint'))
 let shortenedurl
+let keywords
 
-const getH2 = (html) => {
+const getSections = (html) => {
   const $ = cheerio.load(html)
-  const headings = []
-  $('h2').each((idx, heading) => {
-    headings.push($(heading).text())
+  const sections = []
+  $('section').each((idx, section) => {
+    sections.push({title: $(section).children('h2').first().text(), text: $(section).text()})
   })
 
-  return headings
+  return sections
 }
 
 const createTemplateObject = async (url, title, summary, postafter) => {
@@ -26,26 +27,57 @@ const addHours = (hours, date) => {
   return new Date(time + hours * 3600000)
 }
 
-const hashtag = (keywords) =>
-  keywords.split(', ').map((keyword) => '#' + keyword.split(' ').join(''))
+const hashtag = (keywords) => {
+  let array
+  typeof keywords === 'string' ? array = keywords.split(', ') : array = keywords
+
+  return array.map((keyword) => '#' + keyword.split(' ').join('')).join(' ')
+}
+
+const findKeywords = (str) =>{
+  const text = str.toLowerCase()
+  return keywords.filter((keyword) => {
+    return text.indexOf(keyword.toLowerCase()) > -1
+  })
+}
 
 const twitterTemplates = async (html, meta, url) => {
   const templates = []
 
-  let summary = `${meta.title}\n${hashtag(meta.keywords).join(' ')}\n${shortenedurl}`
+  let summary = `${meta.title}\n${hashtag(meta.keywords)}\n${shortenedurl}`
   templates.push(await createTemplateObject(url, '', summary))
 
   summary = `${meta.description}\n#Microsoft #Office365 #Technology\n${shortenedurl}`
   templates.push(await createTemplateObject(url, '', summary, addHours(5)))
 
-  getH2(html).forEach((heading) => {
-    console.log(heading)
+  await getSections(html).forEach(async (section, idx) => {
+    if (idx < 4) {
+      const summary = `${section.title}\n${hashtag(findKeywords(section.text))}\n${shortenedurl}`
+
+      let hours
+      switch(idx) {
+      case 0:
+        hours = addHours(24)
+        break
+      case 1:
+        addHours(168)
+        break
+      case 2:
+        addHours(730)
+        break
+      case 3:
+        break
+      }
+
+      templates.push(await createTemplateObject(url, '', summary, hours))
+    }
   })
 
   return templates
 }
 
 const createSocialMediaTemplates = async (templatePath) => {
+  keywords = (await Keywords.get()).map((keyword) => keyword.Title)
   const html = renderTemplate(templatePath)
   const meta = getMeta(templatePath)
   const url = getAbsoluteUrl(meta)
