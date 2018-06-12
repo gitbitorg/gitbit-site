a391bbfa2d641c8bdebdb74255395eeb17385770
John Gruber <john.gruber@gitbit.org>
2018-06-12 19:40:10 -0400

    created finalize

---
+++ src/articles/power-users-guide-to-outlook-2.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'title': 'Power Users Guide to Outlook 2016 Search',
+  'description': 'Despite new apps like Slack, and Microsoft Teams email is still one of the most common and fastest growing platforms for communication. Mailboxes continue to expand, and people are receiving more emails per day year over year. The rapid growth of mailboxes is why Microsoft offers 50 GB mailboxes for',
+  'datePublished': '2018-06-12T04:00:00.000Z',
+  'dateModified': '2018-06-12T04:00:00.000Z',
+  'image1200x1200': 'power-users-guide-to-outlook-2-1200x1200.jpg',
+  'image1200x900': 'power-users-guide-to-outlook-2-1200x900.jpg',
+  'image1200x675': 'power-users-guide-to-outlook-2-1200x675.jpg',
+  'image400x300': 'power-users-guide-to-outlook-2-400x300.jpg',
+  'document': 'power-users-guide-to-outlook-2.docx'
+}
