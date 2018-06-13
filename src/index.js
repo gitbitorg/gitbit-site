78b28e1e3af08dfbf42ae3ecb2cd86e3e2bbb168
John Gruber <john.gruber@gitbit.org>
2018-06-13 14:43:34 -0400

    added canonical, added articles to index, fixed 400x300

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+const Glob = require('glob').Glob
+const {found} = Glob(`${__dirname}\\articles\\*.js`, {sync:true})
+
+const articles = found.map((file) => require(file)).sort((a, b) => new Date(a.dateModified) < new Date(b.dateModified) ? 1 : -1)
 module.exports = {
-  template: 'index.pug'
+  template: 'index.pug',
+  canonical: '/',
+  articles
 }
