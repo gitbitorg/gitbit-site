1aef064d84d535d697787a53c28365e48f15d3bb
John Gruber <john.gruber@gitbit.org>
2018-06-14 16:48:37 -0400

    added legal

---
+++ src/legal.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'template': '/articles/article.pug',
+  canonical: '/legal',
+  title: 'GitBit Privacy & Terms',
+  description: 'Legal - Privacy & Terms',
+  'document': '/assets/legal.docx',
+  datePublished: '2018-01-20T00:00:00.000Z',
+  dateModified: '2018-06-14T00:00:00.000Z',
+  image1200x1200: '/assets/gitbit/gitbit-icon-1200x1200.png',
+  image1200x900: '/assets/gitbit/gitbit-logo-1200x900.png',
+  image1200x675: '/assets/gitbit/gitbit-logo-1200x675.png',
+  image400x300: '/assets/gitbit/gitbit-logo-400x300.png'
+}
