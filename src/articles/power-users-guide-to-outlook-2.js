7450e101c212292047188c509048d06fcbc3140b
John Gruber <john.gruber@gitbit.org>
2018-06-13 22:11:40 -0400

    added apps layout

--- src/articles/power-users-guide-to-outlook-2.js
+++ src/articles/power-users-guide-to-outlook-2.js
@@ -3,7 +3,7 @@ module.exports = {
   'description': 'Despite new apps like Slack, and Microsoft Teams email is still one of the most common and fastest growing platforms for communication. Mailboxes continue to expand, and people are receiving more emails per day year over year. The rapid growth of mailboxes is why Microsoft offers 50 GB mailboxes for',
   'datePublished': '2018-06-13T04:00:00.000Z',
   'dateModified': '2018-06-13T04:00:00.000Z',
-  'template': 'article.template.pug',
+  'template': '/articles/article.template.pug',
   'canonical': '/articles/power-users-guide-to-outlook-2',
   'image1200x1200': '/articles/assets/power-users-guide-to-outlook-2-1200x1200.jpg',
   'image1200x900': '/articles/assets/power-users-guide-to-outlook-2-1200x900.jpg',
