4f33caaafab72d8e413685cff319362cbc61a2fe
John Gruber <john.gruber@gitbit.org>
2018-06-14 10:01:32 -0400

    added article to src

---
+++ src/articles/agile-teamwork-with-office-365.js
@@ -0,0 +1,13 @@
+module.exports = {
+	"title": "Agile Teamwork with Office 365 Planner",
+	"description": "On December 1st, 1913 Henry Ford installed the first moving assembly line. It was brilliant. His innovation reduced the time it took to build a car from more than 12 hours to two hours and 30 minutes. Ford increased production and decreased costs. His plan moved the automobile from one station to th",
+	"datePublished": "2018-06-14T04:00:00.000Z",
+	"dateModified": "2018-06-14T04:00:00.000Z",
+	"template": "/articles/article.template.pug",
+	"canonical": "/articles/agile-teamwork-with-office-365",
+	"image1200x1200": "/articles/assets/agile-teamwork-with-office-365-1200x1200.jpg",
+	"image1200x900": "/articles/assets/agile-teamwork-with-office-365-1200x900.jpg",
+	"image1200x675": "/articles/assets/agile-teamwork-with-office-365-1200x675.jpg",
+	"image400x300": "/articles/assets/agile-teamwork-with-office-365-400x300.jpg",
+	"document": "/articles/assets/agile-teamwork-with-office-365.docx"
+}
