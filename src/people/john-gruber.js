7b3aa15c5aeac779a6a91bd45046ff8eb30a7c8d
John Gruber <john.gruber@gitbit.org>
2018-06-14 12:21:25 -0400

    added people page

---
+++ src/people/john-gruber.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'title': 'John Gruber',
+  'description': 'John Gruber is a Collaboration Engineer and is responsible for helping customers manage and maintain Microsoft Office 365. John brings 10 years of information technology (IT) experience, helping small and large organizations across North America. Follow him on Twitter @gruberjl',
+  'datePublished': '2018-04-28T02:56:00.998Z',
+  'dateModified': '2018-04-28T02:56:00.999Z',
+  'canonical': '/people/john-gruber',
+  'image1200x1200': '/people/assets/john-gruber-1200x1200.jpg',
+  'image1200x900': '/people/assets/john-gruber-1200x900.jpg',
+  'image1200x675': '/people/assets/john-gruber-1200x675.jpg',
+  'image400x300': '/people/assets/john-gruber-400x300.jpg',
+  'template': '/people/people.pug'
+}
