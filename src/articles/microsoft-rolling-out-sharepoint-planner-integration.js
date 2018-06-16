0b2ea7c552bc574954c520b8be486f367f9fe4a6
John Gruber <john.gruber@gitbit.org>
2018-06-16 19:04:17 -0400

    added article

---
+++ src/articles/microsoft-rolling-out-sharepoint-planner-integration.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'title': 'Microsoft Rolling Out SharePoint Planner Integration',
+  'description': 'Microsoft has announced they are rolling out SharePoint and Planner integration. Office 365 customers will start to see the new integration in their SharePoint sites shortly. The new integration will further integrate the separate Office 365 apps into one streamlined toolset aiding people in managem',
+  'datePublished': '2018-06-16T04:00:00.000Z',
+  'dateModified': '2018-06-16T04:00:00.000Z',
+  'template': '/articles/article.pug',
+  'canonical': '/articles/microsoft-rolling-out-sharepoint-planner-integration',
+  'image1200x1200': '/articles/assets/microsoft-rolling-out-sharepoint-planner-integration-1200x1200.jpg',
+  'image1200x900': '/articles/assets/microsoft-rolling-out-sharepoint-planner-integration-1200x900.jpg',
+  'image1200x675': '/articles/assets/microsoft-rolling-out-sharepoint-planner-integration-1200x675.jpg',
+  'image400x300': '/articles/assets/microsoft-rolling-out-sharepoint-planner-integration-400x300.jpg',
+  'document': '/articles/assets/microsoft-rolling-out-sharepoint-planner-integration.docx'
+}
