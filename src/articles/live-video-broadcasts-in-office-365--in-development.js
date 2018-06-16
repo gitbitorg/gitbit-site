99d64573c311872addf0104b9a5cecf40127ab64
John Gruber <john.gruber@gitbit.org>
2018-06-15 22:07:40 -0400

    added article

---
+++ src/articles/live-video-broadcasts-in-office-365--in-development.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'title': 'Live Video Broadcasts in Office 365 – In Development',
+  'description': 'Microsoft has begun development on live video broadcasting for Office 365. Live video broadcasting refers to online streaming media simultaneously recorded and broadcast in real time to the viewer. It’s referred to as streaming. The new feature will be included in Microsoft Stream, Office 365’s vide',
+  'datePublished': '2018-06-16T04:00:00.000Z',
+  'dateModified': '2018-06-16T04:00:00.000Z',
+  'template': '/articles/article.pug',
+  'canonical': '/articles/live-video-broadcasts-in-office-365--in-development',
+  'image1200x1200': '/articles/assets/live-video-broadcasts-in-office-365--in-development-1200x1200.jpg',
+  'image1200x900': '/articles/assets/live-video-broadcasts-in-office-365--in-development-1200x900.jpg',
+  'image1200x675': '/articles/assets/live-video-broadcasts-in-office-365--in-development-1200x675.jpg',
+  'image400x300': '/articles/assets/live-video-broadcasts-in-office-365--in-development-400x300.jpg',
+  'document': '/articles/assets/live-video-broadcasts-in-office-365--in-development.docx'
+}
